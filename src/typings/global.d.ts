09f463b721fb996b06363758ba641301b7060aa2
Halsey <$>
2022-04-12 14:04:57 +0800

    fea：新增window.d.ts

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -23,13 +23,3 @@ declare interface ViteEnv {
 	VITE_BUILD_GZIP: boolean;
 	VITE_REPORT: boolean;
 }
-
-// * global
-declare global {
-	interface Navigator {
-		msSaveOrOpenBlob: (blob: Blob, fileName: string) => void;
-		browserLanguage: string;
-	}
-}
-
-export {};
