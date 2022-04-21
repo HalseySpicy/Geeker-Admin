89bbc99ba2cc376dd2c06b4026bf07d25ed00378
Halsey <$>
2022-04-21 15:31:32 +0800

    delete：去掉vite图片压缩

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { resolve } from "path";
 import { wrapperEnv } from "./src/utils/getEnv";
 import { visualizer } from "rollup-plugin-visualizer";
 import viteCompression from "vite-plugin-compression";
-import viteImagemin from "vite-plugin-imagemin";
 import VueSetupExtend from "vite-plugin-vue-setup-extend";
+// import viteImagemin from "vite-plugin-imagemin";
 // import importToCDN from "vite-plugin-cdn-import";
 // import AutoImport from "unplugin-auto-import/vite";
 // import Components from "unplugin-vue-components/vite";
@@ -58,14 +58,14 @@ export default defineConfig((mode: ConfigEnv): UserConfig => {
 					}
 				}
 			}),
-			// demand import element（如果使用了cdn引入,没必要使用element自动导入了）
+			// * demand import element（如果使用了cdn引入,没必要使用element自动导入了）
 			// AutoImport({
 			// 	resolvers: [ElementPlusResolver()]
 			// }),
 			// Components({
 			// 	resolvers: [ElementPlusResolver()]
 			// }),
-			// cdn 引入（vue、element-plus）
+			// * cdn 引入（vue、element-plus）
 			// importToCDN({
 			// 	modules: [
 			// 		{
@@ -81,37 +81,37 @@ export default defineConfig((mode: ConfigEnv): UserConfig => {
 			// 		}
 			// 	]
 			// }),
-			// name 可以写在script上
+			// * name 可以写在script上
 			VueSetupExtend(),
-			// image compress
-			viteImagemin({
-				gifsicle: {
-					optimizationLevel: 7,
-					interlaced: false
-				},
-				optipng: {
-					optimizationLevel: 7
-				},
-				mozjpeg: {
-					quality: 20
-				},
-				pngquant: {
-					quality: [0.8, 0.9],
-					speed: 4
-				},
-				svgo: {
-					plugins: [
-						{
-							name: "removeViewBox"
-						},
-						{
-							name: "removeEmptyAttrs",
-							active: false
-						}
-					]
-				}
-			}),
-			// gzip compress
+			// * image compress(测试了下这个插件，没啥用，反而图片压缩过后变大了，打包的时候还巨慢)
+			// viteImagemin({
+			// 	gifsicle: {
+			// 		optimizationLevel: 7,
+			// 		interlaced: false
+			// 	},
+			// 	optipng: {
+			// 		optimizationLevel: 7
+			// 	},
+			// 	mozjpeg: {
+			// 		quality: 20
+			// 	},
+			// 	pngquant: {
+			// 		quality: [0.8, 0.9],
+			// 		speed: 4
+			// 	},
+			// 	svgo: {
+			// 		plugins: [
+			// 			{
+			// 				name: "removeViewBox"
+			// 			},
+			// 			{
+			// 				name: "removeEmptyAttrs",
+			// 				active: false
+			// 			}
+			// 		]
+			// 	}
+			// }),
+			// * gzip compress
 			viteEnv.VITE_BUILD_GZIP &&
 				viteCompression({
 					verbose: true,
@@ -120,7 +120,7 @@ export default defineConfig((mode: ConfigEnv): UserConfig => {
 					algorithm: "gzip",
 					ext: ".gz"
 				}),
-			// 是否生成包预览
+			// * 是否生成包预览
 			viteEnv.VITE_REPORT && visualizer()
 		],
 		// build configure
