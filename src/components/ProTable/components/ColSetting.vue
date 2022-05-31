a3b86a06a6d9cd4b6f7ac6e108727a0b4852e9a0
Halsey <$>
2022-05-31 12:53:07 +0800

    fix: 🧩 修复 Pro-Tabel 列设置 bug

--- src/components/ProTable/components/ColSetting.vue
+++ src/components/ProTable/components/ColSetting.vue
@@ -1,11 +1,11 @@
 <template>
 	<!-- 列显隐设置 -->
-	<el-drawer title="列设置" v-model="drawerVisible" size="400">
+	<el-drawer title="列设置" v-model="drawerVisible" size="400px">
 		<div class="table-box">
-			<el-table :data="colSetting" :border="true">
-				<el-table-column prop="label" label="列名" width="180" />
-				<el-table-column prop="name" label="显示" width="180" v-slot="scope">
-					<el-switch v-model="scope.row.isShow" @change="switchShow"></el-switch>
+			<el-table height="575" :data="colSetting" :border="true">
+				<el-table-column prop="label" label="列名" />
+				<el-table-column prop="name" label="显示" v-slot="scope">
+					<el-switch v-model="scope.row.isShow"></el-switch>
 				</el-table-column>
 				<template #empty>
 					<div class="table-empty">
@@ -19,10 +19,10 @@
 </template>
 
 <script setup lang="ts" name="colSetting">
-import { ref, nextTick } from "vue";
+import { ref } from "vue";
 import { ColumnProps } from "@/components/ProTable/interface";
 
-const props = defineProps<{ colSetting: Partial<ColumnProps>[]; tableRef: any }>();
+defineProps<{ colSetting: Partial<ColumnProps>[]; tableRef: any }>();
 
 const drawerVisible = ref<boolean>(false);
 // 打开列设置
@@ -30,12 +30,12 @@ const openColSetting = (): void => {
 	drawerVisible.value = true;
 };
 
-// 列显隐时重新布局 table
-const switchShow = () => {
-	nextTick(() => {
-		props.tableRef.doLayout();
-	});
-};
+// 列显隐时重新布局 table（虽然切换时表格不抖动了，但是表格会出现横向滚动条）
+// const switchShow = () => {
+// 	nextTick(() => {
+// 		props.tableRef.doLayout();
+// 	});
+// };
 
 defineExpose({
 	openColSetting
