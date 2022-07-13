3598dbc2a83aaacf9dada4e2c38a3ca27cbe4cfd
Halsey <$>
2022-07-13 14:28:29 +0800

    fix: 🧩 修复请求 header 参数丢失 bug

--- src/api/index.ts
+++ src/api/index.ts
@@ -46,7 +46,7 @@ class RequestHttp {
 				// * 如果当前请求不需要显示 loading,在api服务中通过指定的第三个参数: { headers: { noLoading: true } }来控制不显示loading，参见loginApi
 				config.headers!.noLoading || showFullScreenLoading();
 				const token: string = globalStore.token;
-				return { ...config, headers: { "x-access-token": token } };
+				return { ...config, headers: { ...config.headers, "x-access-token": token } };
 			},
 			(error: AxiosError) => {
 				return Promise.reject(error);
