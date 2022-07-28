ee0e73fdc77dc0859c643b745fcf4c0bf8e81e15
Halsey <$>
2022-07-28 17:58:24 +0800

    chore: 🔨 增加本地菜单列表，在 api 中可切换为本地菜单

--- src/api/modules/login.ts
+++ src/api/modules/login.ts
@@ -1,5 +1,6 @@
 import { Login } from "@/api/interface/index";
 import { PORT1 } from "@/api/config/servicePort";
+// import Menu from "@/assets/json/menu.json";
 import qs from "qs";
 
 import http from "@/api";
@@ -23,4 +24,6 @@ export const getAuthButtons = () => {
 // * 获取菜单列表
 export const getMenuList = () => {
 	return http.get<Menu.MenuOptions[]>(PORT1 + `/menu/list`);
+	// 如果想让菜单变为本地数据，注释上一行代码，并引入本地 Menu.json 数据
+	// return Menu;
 };
