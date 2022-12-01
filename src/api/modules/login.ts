7b6943df234404632c66e57962368eb2c06bfadc
Halsey <$>
2022-12-01 11:05:31 +0800

    chore: 🔨 更改菜单请求地址

--- src/api/modules/login.ts
+++ src/api/modules/login.ts
@@ -23,7 +23,7 @@ export const getAuthButtonListApi = () => {
 
 // * 获取菜单列表
 export const getAuthMenuListApi = () => {
-	// return http.get<Menu.MenuOptions[]>(PORT1 + `/menu/list`, {}, { headers: { noLoading: true } });
+	return http.get<Menu.MenuOptions[]>(PORT1 + `/menu/list`, {}, { headers: { noLoading: true } });
 	// 如果想让菜单变为本地数据，注释上一行代码，并引入本地 dynamicRouter.json 数据
 	return DynamicRouter;
 };
