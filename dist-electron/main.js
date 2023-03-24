faaab45539141a33aea57c8fa0774c2408028fad
sylvie <1553902582@qq.com>
2023-03-24 10:31:53 +0800

    规范代码

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,1 +1,6 @@
-"use strict";const n=require("electron");n.app.whenReady().then(()=>{const e=new n.BrowserWindow({title:"Main window"});process.env.VITE_DEV_SERVER_URL?e.loadURL(process.env.VITE_DEV_SERVER_URL):e.loadFile("dist/index.html")});
+"use strict";
+const n = require("electron");
+n.app.whenReady().then(() => {
+	const e = new n.BrowserWindow({ title: "Main window" });
+	process.env.VITE_DEV_SERVER_URL ? e.loadURL(process.env.VITE_DEV_SERVER_URL) : e.loadFile("dist/index.html");
+});
