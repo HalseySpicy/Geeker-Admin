99da244df99fc92a5bd6915abb99d2bcc185598e
sylvie <1553902582@qq.com>
2023-03-28 09:09:19 +0800

    wip: 开发中

--- src/api/modules/login.ts
+++ src/api/modules/login.ts
@@ -1,20 +1,25 @@
 import { Login } from "@/api/interface/index";
-import { PORT1 } from "@/api/config/servicePort";
+// import { PORT1 } from "@/api/config/servicePort";
 import DynamicRouter from "@/assets/json/dynamicRouter.json";
 import AuthButtons from "@/assets/json/authButtons.json";
-import qs from "qs";
-import http from "@/api";
+// import qs from "qs";
+// import http from "@/api";
 
 /**
  * @name 登录模块
  */
 // * 用户登录
 export const loginApi = (params: Login.ReqLoginForm) => {
-	return http.post<Login.ResLogin>(PORT1 + `/login`, params, { headers: { noLoading: true } }); // 正常 post json 请求  ==>  application/json
-	return http.post<Login.ResLogin>(PORT1 + `/login`, params, { headers: { noLoading: true } }); // 控制当前请求不显示 loading
-	return http.post<Login.ResLogin>(PORT1 + `/login`, {}, { params }); // post 请求携带 query 参数  ==>  ?username=admin&password=123456
-	return http.post<Login.ResLogin>(PORT1 + `/login`, qs.stringify(params)); // post 请求携带表单参数  ==>  application/x-www-form-urlencoded
-	return http.get<Login.ResLogin>(PORT1 + `/login?${qs.stringify(params, { arrayFormat: "repeat" })}`); // 如果是 get 请求可以携带数组等复杂参数
+	// return http.post<Login.ResLogin>(PORT1 + `/login`, params, { headers: { noLoading: true } }); // 正常 post json 请求  ==>  application/json
+	// return http.post<Login.ResLogin>(PORT1 + `/login`, params, { headers: { noLoading: true } }); // 控制当前请求不显示 loading
+	// return http.post<Login.ResLogin>(PORT1 + `/login`, {}, { params }); // post 请求携带 query 参数  ==>  ?username=admin&password=123456
+	// return http.post<Login.ResLogin>(PORT1 + `/login`, qs.stringify(params)); // post 请求携带表单参数  ==>  application/x-www-form-urlencoded
+	// return http.get<Login.ResLogin>(PORT1 + `/login?${qs.stringify(params, { arrayFormat: "repeat" })}`); // 如果是 get 请求可以携带数组等复杂参数
+	if (params) {
+		return { code: 200, data: { access_token: "bqddxxwqmfncffacvbpkuxvwvqrhln" }, msg: "成功" }; // token模拟
+	} else {
+		return { code: 200, data: { access_token: "bqddxxwqmfncffacvbpkuxvwvqrhln" }, msg: "成功" }; // token模拟
+	}
 };
 
 // * 获取按钮权限
@@ -33,5 +38,6 @@ export const getAuthMenuListApi = () => {
 
 // * 用户退出登录
 export const logoutApi = () => {
-	return http.post(PORT1 + `/logout`);
+	// return http.post(PORT1 + `/logout`);
+	return { code: 200, msg: "成功" };
 };
