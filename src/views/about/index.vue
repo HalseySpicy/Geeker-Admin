d62aae868e7d6aad87f0ba8706ab9744d5da04dd
HalseySpicy <$>
2023-05-24 18:13:30 +0800

    feat: 🚀 update stylelint configuration

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -28,7 +28,7 @@
           <el-link type="primary" href="https://docs.spicyboy.cn" target="_blank">文档地址</el-link>
         </el-descriptions-item>
         <el-descriptions-item label="预览地址" label-align="left">
-          <el-link type="primary" href="http://admin.spicyboy.cn" target="_blank">预览地址</el-link>
+          <el-link type="primary" href="https://admin.spicyboy.cn" target="_blank">预览地址</el-link>
         </el-descriptions-item>
       </el-descriptions>
     </div>
