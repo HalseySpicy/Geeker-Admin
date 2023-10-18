f08a1256b35717a253ebb28c690b9b1c06352fc1
lean <efilehtyojne@qq.com>
2023-10-18 18:17:45 +0800

    fix(directive): 🧩 removeEventListener parameter is wrong
    el.__handleClick__ 没有指向添加监听器时的同一个函数地址，导致移除监听器失败

--- src/directives/modules/copy.ts
+++ src/directives/modules/copy.ts
@@ -3,11 +3,11 @@
  * 复制某个值至剪贴板
  * 接收参数：string类型/Ref<string>类型/Reactive<string>类型
  */
+
 import type { Directive, DirectiveBinding } from "vue";
 import { ElMessage } from "element-plus";
 interface ElType extends HTMLElement {
   copyData: string | number;
-  __handleClick__: any;
 }
 const copy: Directive = {
   mounted(el: ElType, binding: DirectiveBinding) {
@@ -18,7 +18,7 @@ const copy: Directive = {
     el.copyData = binding.value;
   },
   beforeUnmount(el: ElType) {
-    el.removeEventListener("click", el.__handleClick__);
+    el.removeEventListener("click", handleClick);
   }
 };
 
