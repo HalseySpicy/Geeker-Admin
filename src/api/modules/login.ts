a7857cd3f932e5e5ed334e48a76e494a436cebac
crott <zhang_jiexu@163.coom>
2024-04-17 20:47:12 +0800

    feat(view): 🚀 init project
    init project, remove some unuse files

--- src/api/modules/login.ts
+++ src/api/modules/login.ts
@@ -18,7 +18,7 @@ export const loginApi = (params: Login.ReqLoginForm) => {
 
 // 获取菜单列表
 export const getAuthMenuListApi = () => {
-  return http.get<Menu.MenuOptions[]>(PORT1 + `/menu/list`, {}, { loading: false });
+  // return http.get<Menu.MenuOptions[]>(PORT1 + `/menu/list`, {}, { loading: false });
   // 如果想让菜单变为本地数据，注释上一行代码，并引入本地 authMenuList.json 数据
   return authMenuList;
 };
