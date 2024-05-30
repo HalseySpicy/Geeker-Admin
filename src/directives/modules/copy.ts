49418cf410d8539b4641711d6d42e297bd4affdb
fanlong01 <fanlong01@longfor.com>
2024-05-30 12:57:26 +0800

    fix: 修复复制指令卸载钩子中不解绑回调函数问题

--- src/directives/modules/copy.ts
+++ src/directives/modules/copy.ts
@@ -7,7 +7,6 @@ import type { Directive, DirectiveBinding } from "vue";
 import { ElMessage } from "element-plus";
 interface ElType extends HTMLElement {
   copyData: string | number;
-  __handleClick__: any;
 }
 const copy: Directive = {
   mounted(el: ElType, binding: DirectiveBinding) {
@@ -18,7 +17,7 @@ const copy: Directive = {
     el.copyData = binding.value;
   },
   beforeUnmount(el: ElType) {
-    el.removeEventListener("click", el.__handleClick__);
+    el.removeEventListener("click", handleClick);
   }
 };
 
