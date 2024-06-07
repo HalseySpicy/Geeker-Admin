e4176a7655eceae167dcee2eaa61dc526d3ac71a
wangsheng <uamr567@sina.com>
2024-06-07 10:28:48 +0800

    fix: 修复详情页面不能正确缓存的问题

--- src/stores/modules/tabs.ts
+++ src/stores/modules/tabs.ts
@@ -33,11 +33,10 @@ export const useTabsStore = defineStore({
           router.push(nextTab.path);
         });
       }
-      // remove keepalive
-      const tabItem = this.tabsMenuList.find(item => item.path === tabPath);
-      tabItem?.isKeepAlive && keepAliveStore.removeKeepAliveName(tabItem.name);
       // set tabs
       this.tabsMenuList = this.tabsMenuList.filter(item => item.path !== tabPath);
+      // set keepalive
+      this.setKeepAliveName();
     },
     // Close Tabs On Side
     async closeTabsOnSide(path: string, type: "left" | "right") {
@@ -49,17 +48,14 @@ export const useTabsStore = defineStore({
         });
       }
       // set keepalive
-      const KeepAliveList = this.tabsMenuList.filter(item => item.isKeepAlive);
-      keepAliveStore.setKeepAliveName(KeepAliveList.map(item => item.name));
+      this.setKeepAliveName();
     },
     // Close MultipleTab
     async closeMultipleTab(tabsMenuValue?: string) {
       this.tabsMenuList = this.tabsMenuList.filter(item => {
         return item.path === tabsMenuValue || !item.close;
       });
-      // set keepalive
-      const KeepAliveList = this.tabsMenuList.filter(item => item.isKeepAlive);
-      keepAliveStore.setKeepAliveName(KeepAliveList.map(item => item.name));
+      this.setKeepAliveName();
     },
     // Set Tabs
     async setTabs(tabsMenuList: TabsMenuProps[]) {
@@ -70,6 +66,12 @@ export const useTabsStore = defineStore({
       this.tabsMenuList.forEach(item => {
         if (item.path == getUrlWithParams()) item.title = title;
       });
+    },
+    // Set keepalive
+    async setKeepAliveName() {
+      // set keepalive
+      const KeepAliveList = this.tabsMenuList.filter(item => item.isKeepAlive);
+      keepAliveStore.setKeepAliveName(Array.from(new Set(KeepAliveList.map(item => item.name))));
     }
   },
   persist: piniaPersistConfig("geeker-tabs")
