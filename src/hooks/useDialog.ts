553a0d5e9efa0ec5787f2868235285c384144ce1
zhaoyuchao <928597678@qq.com>
2024-09-10 14:29:53 +0800

    feat(dialog): 添加 useDialog hooks，封装 el-dialog 弹框

---
+++ src/hooks/useDialog.ts
@@ -0,0 +1,127 @@
+import { ElDialog } from "element-plus";
+import type { ComponentInternalInstance, Ref } from "vue";
+import { getCurrentInstance, h, isRef, onUnmounted, render } from "vue";
+import { upperFirst } from "lodash";
+import merge from "lodash/merge";
+
+type Content = Parameters<typeof h>[0] | string | JSX.Element;
+// 使用 InstanceType 获取 ElDialog 组件实例的类型
+type ElDialogInstance = InstanceType<typeof ElDialog>;
+
+// 从组件实例中提取 Props 类型
+type DialogProps = ElDialogInstance["$props"] & {
+  onBeforeOpen?: () => boolean | void; // 支持显示之前钩子
+};
+
+interface ElDialogSlots {
+  header?: (...args: any[]) => Content;
+  footer?: (...args: any[]) => Content;
+}
+
+interface Options<P> {
+  dialogProps?: DialogProps;
+  dialogSlots?: ElDialogSlots;
+  contentProps?: P;
+  closeEventName?: string;
+}
+
+// 定义工具函数，获取计算属性的option
+function getOptions<P>(options?: Ref<Options<P>> | Options<P>) {
+  if (!options) return {};
+  return isRef(options) ? options.value : options;
+}
+
+export function useDialog<P = any>(content: Content, options?: Ref<Options<P>> | Options<P>) {
+  let dialogInstance: ComponentInternalInstance | null = null;
+  let fragment: Element | null = null;
+
+  // 关闭并卸载组件
+  const closeAfter = () => {
+    if (fragment) {
+      render(null, fragment as unknown as Element); // 卸载组件
+      fragment.textContent = ""; // 清空文档片段
+      fragment = null;
+    }
+    dialogInstance = null;
+  };
+
+  // 关闭对话框
+  function closeDialog() {
+    if (dialogInstance) dialogInstance.props.modelValue = false;
+  }
+
+  // 获取当前组件实例，用于设置当前dialog的上下文，继承prototype
+  const instance = getCurrentInstance();
+
+  // 创建并挂载组件
+  function openDialog(modifyOptions?: Partial<Options<P>>) {
+    if (dialogInstance) {
+      closeDialog();
+      closeAfter();
+    }
+    const _options = getOptions(options);
+    // 如果有修改，则合并options。替换之前的options变量为 _options
+    if (modifyOptions) merge(_options, modifyOptions);
+    const { dialogProps, contentProps } = _options;
+
+    // 调用before钩子，如果为false则不打开
+    if (dialogProps?.onBeforeOpen?.() === false) {
+      return;
+    }
+    fragment = document.createDocumentFragment() as unknown as Element;
+
+    // 转换closeEventName事件
+    const closeEventName = `on${upperFirst(_options?.closeEventName || "closeDialog")}`;
+
+    // 定义当前块关闭前钩子变量
+    let onBeforeClose: (() => Promise<boolean | void> | boolean | void) | null;
+
+    const vNode = h(
+      ElDialog,
+      {
+        ...dialogProps,
+        modelValue: true,
+        beforeClose: async done => {
+          // 配置`el-dialog`的关闭回调钩子函数
+          const result = await onBeforeClose?.();
+          if (result === false) {
+            return;
+          }
+          done();
+        },
+        onClosed: () => {
+          dialogProps?.onClosed?.();
+          closeAfter();
+          // 关闭后回收当前变量
+          onBeforeClose = null;
+        }
+      },
+      {
+        default: () => [
+          typeof content === "string"
+            ? content
+            : h(content as any, {
+                ...contentProps,
+                [closeEventName]: closeDialog, // 监听自定义关闭事件，并执行关闭
+                beforeCloseDialog: (fn: () => boolean | void) => {
+                  // 把`beforeCloseDialog`传递给`content`，当组件内部使用`props.beforeCloseDialog(fn)`时，会把fn传递给`onBeforeClose`
+                  onBeforeClose = fn;
+                }
+              })
+        ],
+        ..._options.dialogSlots
+      }
+    );
+    // 设置当前的上下文为使用者的上下文
+    vNode.appContext = instance?.appContext || null;
+    render(vNode, fragment);
+    dialogInstance = vNode.component;
+    document.body.appendChild(fragment);
+  }
+
+  onUnmounted(() => {
+    closeDialog();
+  });
+
+  return { openDialog, closeDialog };
+}
---
+++ src/views/assembly/dialog/useDialog/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card content-box">
+    <span class="text"> useDialog hooks 操作弹框🍓🍇🍈🍉</span>
+
+    <el-button type="primary" @click="openDialog">打开弹框</el-button>
+
+    <div>
+      <span class="title">useDialog hooks介绍 📚</span>
+      <p>
+        在 Vue 中，Hook 允许在函数式组件或者 API 中「钩入」Vue 特性。它们通常在组合式 API（Composition API）中使用，这是 Vue
+        提供的一套响应式和可复用逻辑功能的集合。 本文提到的 useDialog Hook 就是一个封装了 el-dialog 组件基本功能的自定义
+        Hook，它还可以提供附加特性以便在项目中管理和展示弹窗。
+      </p>
+      <span class="title">主要实现功能📚</span>
+      <ul>
+        <li>满足基础用法，传入 el-dialog 的基础属性以及默认slot显示的内容，导出 openDialog 和 closeDialog 函数；</li>
+        <li>支持 el-dialog 的事件配置；</li>
+        <li>支持默认 slot 组件的属性配置；</li>
+        <li>支持 el-dialog 其他 slot 配置，例如 header 和 footer 等；</li>
+        <li>在内容组件中抛出特定事件支持关闭 dialog；</li>
+        <li>支持显示内容为 jsx、普通文本、Vue Component；</li>
+        <li>支持在显示内容中控制是否可以关闭的回调函数，例如 beforeClose；</li>
+        <li>支持显示之前钩子，例如 onBeforeOpen；</li>
+        <li>支持定义和弹出时修改配置属性；</li>
+        <li>支持继承 root vue 的 prototype，可以使用如 vue-i18n 的 $t 函数；</li>
+        <li>支持 ts 参数提示；</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" name="useDialog" setup>
+import { defineAsyncComponent } from "vue";
+import { useDialog } from "@/hooks/useDialog";
+
+const test = defineAsyncComponent(() => import("./test.vue"));
+
+const { openDialog } = useDialog(test, {
+  dialogProps: {
+    title: "测试弹窗",
+    width: "800px"
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
